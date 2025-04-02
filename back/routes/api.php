241d78cf0f9e8c9cf159e8019892d1a52bb9ba34
Jhonatan <jhonatan.dias@dommus.tec.br>
2025-04-02 16:50:26 -0300

    adição das rotas da API

--- back/routes/api.php
+++ back/routes/api.php
@@ -17,3 +17,23 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'destination'], function (){
+    Route::get('/', 'App\Http\Controllers\DestinationController@index');
+    Route::get('/{id}', 'App\Http\Controllers\DestinationController@show');
+    Route::post('/', 'App\Http\Controllers\DestinationController@store');
+    Route::put('/{id}', 'App\Http\Controllers\DestinationController@update');
+    Route::delete('/{id}', 'App\Http\Controllers\DestinationController@destroy');
+});
+
+Route::group(['prefix' => 'plan'], function (){
+    Route::get('/', 'App\Http\Controllers\PlanController@index');
+    Route::get('/{id}', 'App\Http\Controllers\PlanController@show');
+    Route::post('/', 'App\Http\Controllers\PlanController@store');
+    Route::put('/{id}', 'App\Http\Controllers\PlanController@update');
+    Route::delete('/{id}', 'App\Http\Controllers\PlanController@destroy');
+});
+
+Route::group(['prefix' => 'calculator'], function (){
+    Route::get('/', 'App\Http\Controllers\CalculatorController@calculate');
+});
